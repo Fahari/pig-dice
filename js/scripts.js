8ccb48c4fc082b73f4d82acc6fe97422da598f0e
Fahari <karonjekevin67@gmail.com>
2018-12-17 17:54:40 +0300

    add a JavaScript code that makes the buttons functional while forming a random number

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,44 @@ $(document).ready(function() {
   $(".btn-game").click(function() {
     // Put condition when new game button is clicked
     //reset value to 0 and default
-  reset();
+  //reset();
+  alert("hello");
   });
 
-  $('.btn-roll').click(function() {
+  $(".btn-roll").click(function() {
     // Put condition when dice is rolled / clicked
     //function to generate random numbers
-
+      function diceRoll();
+      //alert("hello there");
   });
+ });
+//   $('.btn-hold').click(function() {
+//     // Put condition when hold button is clicked
+//     //Allocate points
+//
+//   });
+//
+//
+//
+//
+// //Business Logic
+//
+// //create function to generate random numbers
+function diceRoll(){
+  var random = Math.floor(Math.random() * 6) + 1;
 
-  $('.btn-hold').click(function() {
-    // Put condition when hold button is clicked
-    //Allocate points
+  if(random ===1){
+    $("#output").html("Sorry!You have rolled a 1. Player's 2 turn!");
+    $("#player1-total").val(); = 0;
+  }else{
+    var currentPlayer1 = parseInt($("#player1-total").val(););
+    addTot1(currentPlayer, random);
+    //$("#player1-total").html("You have rolled " + random);
+    alert(currentPlayer1);
+  }
+};
 
-  });
-
-  });
-
-
-
-//create function to generate random numbers
-
-
+//
 
 // another function to add to scores
 
