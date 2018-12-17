d25d534221caa4bc7575b89f29901a8a10817c60
Fahari <karonjekevin67@gmail.com>
2018-12-17 15:21:18 +0300

    add all the basic NEW code in JavaScript that will make it functional

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $(".btn-game").click(function() {
+    // Put condition when new game button is clicked
+    //reset value to 0 and default
+  reset();
+  });
+
+  $('.btn-roll').click(function() {
+    // Put condition when dice is rolled / clicked
+    //function to generate random numbers
+
+  });
+
+  $('.btn-hold').click(function() {
+    // Put condition when hold button is clicked
+    //Allocate points
+
+  });
+
+  });
+
+
+
+//create function to generate random numbers
+
+
+
+// another function to add to scores
+
+
+
+// another function to add rolled value and score to total points
