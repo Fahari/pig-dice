f4457fec97fc787d2f8a58fbcf23fcf8fe747610
Fahari <karonjekevin67@gmail.com>
2018-12-17 09:51:56 +0300

    completely changed  the css and javascript code

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function rollDice(){
-  var dieOne = $("#die1").val();
-   var output = $("#output").val();
-
-   var d1 = Math.floor(Math.random() * 6) + 1;
-   $("#die1").html(d1);
-   $("#output").html("You rolled " + d1);
-};
